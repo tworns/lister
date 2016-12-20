74d2167582aedc357857cf88be3be073e969a4fd
tworns <tworns@yahoo.com>
2016-12-19 20:19:37 -0500

    Basic item add function implemented

--- app/app.js
+++ app/app.js
@@ -1,8 +1,22 @@
 {
   angular.module('ListController',[])
   .controller('ListController', function($scope){
-
-    //list code
+    $scope.editing = false;
+    //create list of movies
+    $scope.movies = [
+      {title: 'Jaws', year: '1975', genre: 'thriller'}
+    ];
+    //movie up for editing
+    $scope.currentMovie = {title: '', year: '', genre:''};
+    $scope.addMove = function(){
+      if($scope.movie.title.trim !== ''){
+        $scope.movies.push($scope.movie);
+        $scope.currentMovie = {title: '', year: '', genre:''};
+      }
+      else {
+        //error
+      }
+    }
 
 
   });
