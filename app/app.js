e86762fce3d4a764f1e3c3af67e8e6ccc9eeb2f5
tworns <tworns@yahoo.com>
2016-12-19 21:12:31 -0500

    Removed bullet points from list elements, added editing functionality

--- app/app.js
+++ app/app.js
@@ -4,24 +4,39 @@
     $scope.editing = false;
     //create list of movies
     $scope.movies = [
-      {title: 'Jaws', year: '1975', genre: 'thriller'}
+      //{title: 'Jaws', year: '1975', genre: 'thriller'}
     ];
+    $scope.errors = [];
     //movie up for editing
     $scope.movie = {title: '', year: '', genre:''};
     //Add a new movie
     $scope.addMovie = function(){
-      if($scope.movie.title.trim !== ''){
+      if($scope.movie.title.trim() !== ''){
         $scope.movies.push($scope.movie);
         $scope.currentMovie = {title: '', year: '', genre:''};
+        $scope.movie = {};
+        $scope.errors = [];
       }
       else {
-        //error
+        //Error silently to user, log to developer console
+        $scope.errors.push("Error: TITLE REQUIRED");
       }
     }
     //remove movie
     $scope.removeMovie = function (index){
       $scope.movies.splice(index,1);
     }
-
+    //edit movie
+    $scope.editMovie = function (movie, index){
+      $scope.editing = true;
+      $scope.movie = movie;
+      $scope.removeMovie(index);
+    }
+    // Update function submits an edited entry
+    $scope.updateMovie = function() {
+      $scope.addMovie();
+      $scope.movie = {};
+      $scope.editing = false;
+    }
 
   });
